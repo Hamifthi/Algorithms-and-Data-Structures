b424b3272a920a08614d5e5630f42a3e33f5a64a
Hamifthi <hamedonil@yahoo.com>
2018-11-06 13:44:09 +0330

    write  essential calculation parts and now the final array is ready to be made

--- strassen_matrix_multiplication_algorithm.py
+++ strassen_matrix_multiplication_algorithm.py
@@ -5,12 +5,38 @@ def matrix_multiplication_algorithm(array_a, array_b):
         # if array_a.shape == (2, 2):
         #     return array_a, array_b
         A1, A2 = np.split(array_a, 2, axis = 0)
-        A11, A12 = np.split(A1, 2, axis = 1)
-        A21, A22 = np.split(A2, 2, axis = 1)
+        A, B = np.split(A1, 2, axis = 1)
+        C, D = np.split(A2, 2, axis = 1)
         B1, B2 = np.split(array_b, 2, axis = 0)
-        B11, B12 = np.split(B1, 2, axis = 1)
-        B21, B22 = np.split(B2, 2, axis = 1)
-        return A11, A12, A21, A22, B11, B12, B21, B22
+        E, F = np.split(B1, 2, axis = 1)
+        G, H = np.split(B2, 2, axis = 1)
+        return A, B, C, D, E, F, G, H
+
+    def seven_blocks_multiplication(A, B, C, D, E, F, G, H):
+        P1 = A * (F - H)
+        P2 = (A + B) * H
+        P3 = (C + D) * E
+        P4 = D * (G - E)
+        P5 = (A + D) * (E + H)
+        P6 = (B - D) * (G + H)
+        P7 = (A - C) * (E + F)
+        return P1, P2, P3, P4, P5, P6, P7
+
+    def calculate_final_result(P1, P2, P3, P4, P5, P6, P7):
+        F11 = P5 + P4 - P2 + P6
+        F12 = P1 + P2
+        F21 = P3 + P4
+        F22 = P1 + P5 - P3 - P7
+        return F11, F12, F21, F22
+
+
+
+
+
+
+
+
+
 
 a = np.arange(0, 4).reshape(2, 2)
 b = np.arange(4, 8).reshape(2, 2)
