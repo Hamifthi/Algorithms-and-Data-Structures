aaed0c8bad1f9c8698a895ea125076cc31cfcbaf
Hamifthi <hamedonil@yahoo.com>
2018-11-07 15:22:51 +0330

    i add recursion part to the algorithm but caculation of final part has bug and i must fix it

--- strassen_matrix_multiplication_algorithm.py
+++ strassen_matrix_multiplication_algorithm.py
@@ -1,18 +1,30 @@
 import numpy as np
 
 def matrix_multiplication_algorithm(array_a, array_b):
-    def divide_matrix():
-        # if array_a.shape == (2, 2):
-        #     return array_a, array_b
+    def divide_matrix(array_a, array_b):
+        # cut input arrays in 4 parts and initiate 8 different values
         A1, A2 = np.split(array_a, 2, axis = 0)
         A, B = np.split(A1, 2, axis = 1)
         C, D = np.split(A2, 2, axis = 1)
         B1, B2 = np.split(array_b, 2, axis = 0)
         E, F = np.split(B1, 2, axis = 1)
         G, H = np.split(B2, 2, axis = 1)
-        return A, B, C, D, E, F, G, H
+        # check if the arrays are 2 by 2 and return final output here
+        if array_a.shape == (2, 2):
+            F11, F12, F21, F22 = calculation(A, B, C, D, E, F, G, H)
+            return final_array_result(F11, F12, F21, F22)
 
-    def seven_blocks_multiplication(A, B, C, D, E, F, G, H):
+        # this is the recursion part
+        part1 = divide_matrix(A, E)
+        part2 = divide_matrix(B, F)
+        part3 = divide_matrix(C, G)
+        part4 = divide_matrix(D, H)
+        
+        # return final calculated array
+        return final_array_result(part1, part2, part3, part4)
+
+    def calculation(A, B, C, D, E, F, G, H):
+        # calculating septet P values here
         P1 = A * (F - H)
         P2 = (A + B) * H
         P3 = (C + D) * E
@@ -20,25 +32,24 @@ def matrix_multiplication_algorithm(array_a, array_b):
         P5 = (A + D) * (E + H)
         P6 = (B - D) * (G + H)
         P7 = (A - C) * (E + F)
-        return P1, P2, P3, P4, P5, P6, P7
-
-    def calculate_final_result(P1, P2, P3, P4, P5, P6, P7):
+        # 4 parts of final array calculated here
         F11 = P5 + P4 - P2 + P6
         F12 = P1 + P2
         F21 = P3 + P4
         F22 = P1 + P5 - P3 - P7
+        # now we return the 4 parts of final output array
         return F11, F12, F21, F22
 
+    # assembling final output here
+    def final_array_result(F11, F12, F21, F22):
+        # calculating output array dimensions here
+        n = 2 * F11.shape[0]
+        # return final array here
+        return np.array([F11, F12, F21, F22]).reshape(n, n)
 
-    def final_array_result():
-        A, B, C, D, E, F, G, H = divide_matrix()
-        P1, P2, P3, P4, P5, P6, P7 = seven_blocks_multiplication(A, B, C, D, E, F, G, H)
-        F11, F12, F21, F22 = calculate_final_result(P1, P2, P3, P4, P5, P6, P7)
-        final_array = np.array([F11, F12, F21, F22]).reshape(2, 2)
-        return final_array
-    return final_array_result()
+    return divide_matrix(array_a, array_b)
 
-a = np.arange(0, 4).reshape(2, 2)
-b = np.arange(4, 8).reshape(2, 2)
+a = np.arange(0, 16).reshape(4, 4)
+b = np.arange(16, 32).reshape(4, 4)
 sth = matrix_multiplication_algorithm(a, b)
 print(sth)
