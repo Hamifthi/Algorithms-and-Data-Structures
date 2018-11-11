e8cfd8b2f54e9a3c54b569a1f48bbd020ed328c9
Hamifthi <hamedonil@yahoo.com>
2018-11-11 23:08:28 +0330

    quick sort algorithm is ready

--- quick_sort.py
+++ quick_sort.py
@@ -2,26 +2,37 @@ import numpy as np
 import random
 
 def quick_sort_algorithm(input_array):
-    def partitioning(input_array):
-        input_array = np.array(input_array)
-        pivot = random.choice(input_array)
+
+    def partitioning(input_array, low, high):
+        pivot = random.choice(input_array[low:high])
         pivot_index = np.argwhere(input_array == pivot)[0][0]
-        input_array[0], input_array[pivot_index] = input_array[pivot_index], input_array[0]
-        i = 0
-        high = len(input_array)
-        for j in range(1, high):
-            print(pivot, input_array[j])
+        input_array[low], input_array[pivot_index] = input_array[pivot_index], input_array[low]
+
+        i = low + 1
+        for j in range(low, high):
             if input_array[j] < pivot:
                 input_array[j], input_array[i] = input_array[i], input_array[j]
                 i += 1
             else:
                 continue
-        pivot_index = np.argwhere(input_array == pivot)[0][0]
-        input_array[pivot_index], input_array[i] = input_array[i], input_array[pivot_index]
 
-    return partitioning(input_array)
+        input_array[low], input_array[i - 1] = input_array[i - 1], input_array[low]
+        pivot_index = np.argwhere(input_array == pivot)[0][0]
+        return input_array, pivot_index
+
+    def quick_sort(input_array, low, high):
+
+        if high > low:
+
+            input_array, pivot_index = partitioning(input_array, low, high)
+
+            quick_sort(input_array, low, pivot_index)
+            quick_sort(input_array, pivot_index + 1, high)
+        
+    return quick_sort(input_array, 0, len(input_array))
     
         
-input_array = random.sample(range(12), 12)
+input_array = np.array(random.sample(range(15), 15))
 print(input_array)
 print(quick_sort_algorithm(input_array))
+print(input_array)
